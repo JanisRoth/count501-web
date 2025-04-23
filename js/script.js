f0894db0e572f508aafcbd76b3a44f2342bc4c31
Janis Roth <janis.roth@isolutions.ch>
2025-04-23 09:07:27 +0200

    code cleaning

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-// Function to toggle the menu
 function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("active");
 }
 
-// Add an event listener to close the menu when clicking outside
 document.addEventListener('click', function(event) {
     const navLinks = document.getElementById("navLinks");
     const burgerMenu = document.querySelector(".burger-menu");
     
-    // Check if the menu is active and the click was outside the menu and not on the burger button
     if (navLinks.classList.contains("active") && 
         !navLinks.contains(event.target) && 
         !burgerMenu.contains(event.target)) {
@@ -16,7 +13,6 @@ document.addEventListener('click', function(event) {
     }
 });
 
-// Prevent clicks on the menu itself from closing it
 document.getElementById("navLinks").addEventListener('click', function(event) {
     event.stopPropagation();
 });
